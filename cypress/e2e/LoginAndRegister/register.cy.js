1bb4d039851b533e8e2378dd38666127f781f0d0
Mateusz Kobiera <71442804+MateuszKobiera@users.noreply.github.com>
2025-11-05 08:13:24 +0100

    Add Cypress setup and configurations

---
+++ cypress/e2e/LoginAndRegister/register.cy.js
@@ -0,0 +1,23 @@
+import { faker } from '@faker-js/faker';
+
+describe('Register scenario', () => {
+  beforeEach(() => {
+    cy.visit()
+  })
+
+  it('User can register', () => {
+    const user = {
+      name: faker.person.firstName(),
+      lastName: faker.person.lastName(),
+      email: faker.internet.email(),
+      birthDate: '1996-12-20',
+      password: faker.internet.password()
+    }
+    cy.get('input[data-testid=\'firstname-input\']').should('have.property', 'placeholder', "Enter User First Name").type(user.name)
+    cy.get('input[data-testid=\'lastname-input\']').type(user.lastName)
+    cy.get('input[data-testid=\'email-input\']').type(user.email)
+    cy.get('input[data-testid=\'birthdate-input\']').type(user.birthDate)
+    cy.get('input[data-testid=\'password-input\']').type(user.password)
+    cy.get('input[data-testid=\'register-button\']').click()
+  })
+  });
